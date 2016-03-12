81e1699d83086d87ab824b0128aaa1c41d1aa545
ldion <ldion78@gmail.com>
2016-03-12 15:47:32 -0500

    added distribution code for AI

---
+++ ai.cpp
@@ -0,0 +1,8 @@
+#include "ai.h"
+#include "board.h"
+#include <cstdlib>
+#include <time.h>
+
+int connect4AI(const Board& board){
+    return rand() % 7;
+}
---
+++ ai.h
@@ -0,0 +1,8 @@
+#ifndef AI_H__
+#define AI_H__
+
+#include "board.h"
+
+int connect4AI(const Board& board);
+
+#endif /* AI_H__ */
